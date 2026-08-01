daa8de2358824b11caa58800fc10bb59ff845ec5
M. Vamshi Kumar <vamshikumar172003@gmail.com>
2026-08-01 22:59:02 +0530

    Initial portfolio website

---
+++ js/script.js
@@ -0,0 +1,28 @@
+AOS.init({
+    duration:1000,
+    once:true
+});
+
+new Typed("#typing",{
+
+strings:[
+
+"Electrical Engineering Student",
+
+"Published Research Author",
+
+"Renewable Energy Enthusiast",
+
+"Power Systems Engineer",
+
+"Graduate Engineer Trainee Aspirant"
+
+],
+
+typeSpeed:60,
+
+backSpeed:40,
+
+loop:true
+
+});
