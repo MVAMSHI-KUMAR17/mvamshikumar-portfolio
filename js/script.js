b4c018386b2314fcc60a5cd006d84750b8a6e05f
M. Vamshi Kumar <vamshikumar172003@gmail.com>
2026-08-03 20:53:13 +0530

    Update JavaScript functionality

--- js/script.js
+++ js/script.js
@@ -175,3 +175,21 @@ link.classList.add("active");
 });
 
 });
+const themeBtn = document.getElementById("themeToggle");
+
+themeBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("light-mode");
+
+    localStorage.setItem(
+        "theme",
+        document.body.classList.contains("light-mode")
+        ? "light"
+        : "dark"
+    );
+
+});
+
+if(localStorage.getItem("theme")==="light"){
+    document.body.classList.add("light-mode");
+}
